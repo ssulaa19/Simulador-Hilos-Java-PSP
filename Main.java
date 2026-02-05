8ef3ced3026c591fdc61f28ec4c8a14ce329665a
ssulaa19 <alumno.502966@ies-azarquiel.es>
2026-02-05 21:52:04 +0100

    Main mas limpio y ordenado, he creado dos variables nuevas lista de cajeros y una instancia de la clase Random para calcular el tiempo de espera de los hilos, tambien he creado el metodo inicializaHilos() el cual los inicializa y después hace el join() con el main, y finalmente creo el método imprimirResultadoFinal() que imprime un resumen de las transacciones realizadas

--- Main.java
+++ Main.java
@@ -1,45 +1,92 @@
 package psp.actividadCajeros;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     private static Banco miBanco;
-    private static Cajero cajero1;
-    private static Cajero cajero2;
-    private static Cajero cajero3;
     private static List<Cliente> clientesList;
-    private static Cliente individual;
+    private static List<Thread> hilos;
+    private static List<Cajero> misCajeros;
+    private static final Random r = new Random();
 
     public static void main(String[] args) {
         inicializaVariables();
         System.out.println("Saldo inicial del banco: " + miBanco.getCantidadTotal()+"\n");
-        cajero1.ejecuta(clientesList);
 
-        cajero1.getTransacciones().forEach((x,y) ->{
-            y.forEach(c -> {
-                System.out.println(c.getNombre() +" ha sacado dinero: " + x );
+        inicializaHilos();
+
+        imprimirResultadoFinal();
+    }
+
+    private static Thread getThread(Cajero cajero) {
+        int n = r.nextInt(200, 1201);
+
+        return new Thread(()-> {
+            cajero.ejecuta(clientesList);
+
+            cajero.getTransacciones().forEach((x,y) ->{
+                y.forEach(c -> {
+                    try {
+                        Thread.sleep(n);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println("["+cajero.getNombre()+"] El cliente "+c.getNombre()+" solicita "+ c.getMonto()  +"€, ha sacado dinero: " + x+", tiempo de simulación: " +  n +"ms" );
+                });
             });
         });
-
-        System.out.println("\nSaldo final del banco: " + miBanco.getCantidadTotal());
     }
 
     private static void inicializaVariables() {
         clientesList = new ArrayList<>(Arrays.asList(
-                new Cliente(200, "Ana"),
-                new Cliente(500, "Luis"),
-                new Cliente(50, "Teo"),
-                new Cliente(1500, "Sula")
+                new Cliente(16, "Ana"),
+                new Cliente(120, "Luis"),
+                new Cliente(30, "Teo"),
+                new Cliente(70, "Sula")
         ));
-        individual = new Cliente(100, "Juan");
 
         miBanco = new Banco();
-        cajero1 = new Cajero(miBanco);
-        cajero2 = new Cajero(miBanco);
-        cajero3 = new Cajero(miBanco);
+        Cajero cajero1 = new Cajero(miBanco, "Cajero 1");
+        Cajero cajero2 = new Cajero(miBanco, "Cajero 2");
+
+        Thread t1 = getThread(cajero1);
+        Thread t2 = getThread(cajero2);
+        hilos = new ArrayList<>(Arrays.asList(t1,t2));
+
+        misCajeros = new ArrayList<>(Arrays.asList(cajero1,cajero2));
+
+    }
+
+    private static void inicializaHilos() {
+
+        hilos.forEach(Thread::start);
+
+        hilos.forEach(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+    }
+
+    private static void imprimirResultadoFinal() {
+        int saldoFinal = miBanco.getCantidadTotal();
+
+        int operacionesCorrectas = misCajeros.stream().mapToInt(p -> p.getTransacciones().get(true).size()).sum();
+        int operacionesDenegadas = misCajeros.stream().mapToInt(p -> p.getTransacciones().get(false).size()).sum();
+        int totalRetirado = misCajeros.stream().mapToInt(Cajero::getTotalRetirado).sum();
+        int clientesAtendidos = misCajeros.stream().mapToInt(c -> c.getTransacciones().get(true).size()).sum() +
+                misCajeros.stream().mapToInt(c -> c.getTransacciones().get(false).size()).sum();
+
+        System.out.println("\n \t === Resumen Final === \n");
+        System.out.println("Saldo final: "+ saldoFinal);
+        System.out.println("Operaciones OK: " + operacionesCorrectas);
+        System.out.println("Operaciones Denegadas: " + operacionesDenegadas);
+        System.out.println("Total Retirado: " + totalRetirado);
+        System.out.println("Clientes Atendidos: " + clientesAtendidos);
 
     }
 
