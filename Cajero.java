bef321de950d90fa3ce7ede3aace388d67a126a6
ssulaa19 <alumno.502966@ies-azarquiel.es>
2026-02-05 21:48:21 +0100

    Añado la variable 'totalRetirado' para llevar un control sobre el dinero retirado del banco

--- Cajero.java
+++ Cajero.java
@@ -8,12 +8,16 @@ import java.util.Map;
 public class Cajero {
 
     private Banco miBanco;
-    private Map<Boolean, List<Cliente> > transacciones = new HashMap<>();
+    private final Map<Boolean, List<Cliente> > transacciones = new HashMap<>();
+    private String nombre;
+    private int totalRetirado;
+
 
     public Cajero (){}
 
-    public Cajero (Banco miBanco) {
+    public Cajero (Banco miBanco, String nombre) {
         this.miBanco = miBanco;
+        this.nombre = nombre;
         this.transacciones.put(true, new ArrayList<>());
         this.transacciones.put(false, new ArrayList<>());
 
@@ -25,6 +29,7 @@ public class Cajero {
 
             if (this.miBanco.retirarCantidad(c.getMonto())){
                 transacciones.get(true).add(c);
+                totalRetirado += c.getMonto();
             }
             else {
                 transacciones.get(false).add(c);
@@ -38,13 +43,20 @@ public class Cajero {
 
         if (this.miBanco.retirarCantidad(cliente.getMonto())){
             transacciones.get(true).add(cliente);
+            totalRetirado += cliente.getMonto();
         }
         else {
-            transacciones.get(true).add(cliente);
+            transacciones.get(false).add(cliente);
         }
     }
 
+    protected String getNombre(){
+        return this.nombre;
+    }
+
     protected  Map<Boolean, List<Cliente>> getTransacciones(){return this.transacciones;}
+
+    protected int getTotalRetirado() {return this.totalRetirado;}
 
 
 }
